e79df3a33c7512b3ddf05f502d69f4f3e5ec7fcf
simon de turck <simon.de.turck@philips.com>
2021-03-20 12:55:15 +0100

    Cleanup old class based component and fixed time-travel issue

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Square(props) {
+function Square({ onClick, value }) {
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
+    <button className="square" onClick={onClick}>
+      {value}
     </button>
   );
 }
@@ -35,103 +35,31 @@ function Board({ squares, onClick }) {
   );
 }
 
-// class Game extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       history: [
-//         {
-//           squares: Array(9).fill(null),
-//         },
-//       ],
-//       xIsNext: true,
-//       stepNumber: 0,
-//     };
-//   }
-//   handleOnClick(i) {
-//     const history = this.state.history.slice(0, this.state.stepNumber + 1);
-//     const current = history[history.length - 1];
-//     const sq = current.squares.slice();
-//     if (calculateWinner(sq) || sq[i]) {
-//       return;
-//     }
-//     sq[i] = this.state.xIsNext ? "X" : "O";
-//     this.setState({
-//       history: history.concat([{ squares: sq }]),
-//       xIsNext: !this.state.xIsNext,
-//       stepNumber: history.length,
-//     });
-//   }
-
-//   jumpTo(step) {
-//     this.setState({
-//       stepNumber: step,
-//       xIsNext: step % 2 === 0,
-//     });
-//   }
-//   render() {
-//     const history = this.state.history;
-//     const current = history[this.state.stepNumber];
-//     const winner = calculateWinner(current.squares);
-
-//     const moves = history.map((step, move) => {
-//       const desc = move ? `Go back to move #${move}` : `Go to game start`;
-//       return (
-//         <li key={move}>
-//           <button onClick={() => this.jumpTo(move)}>{desc}</button>
-//         </li>
-//       );
-//     });
-
-//     let status;
-//     if (winner) {
-//       status = `The winner is ${winner}`;
-//     } else {
-//       status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
-//     }
-//     return (
-//       <div className="game">
-//         <div className="game-board">
-//           <Board
-//             squares={current.squares}
-//             onClick={(i) => this.handleOnClick(i)}
-//           />
-//         </div>
-//         <div className="game-info">
-//           <div>{status}</div>
-//           <ol>{moves}</ol>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 function Game() {
   const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
   const [xIsNext, setXIsNext] = useState(true);
   const [stepNumber, setStepNumber] = useState(0);
 
-  const handleOnClick = function (i) {
-    const hist = history.slice(0, stepNumber + 1);
-    const current = hist[history.length - 1];
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const handleOnClick = (i) => {
+    const historyAtStep = history.slice(0, stepNumber + 1);
+    const current = historyAtStep[historyAtStep.length - 1];
     const sq = current.squares.slice();
     if (calculateWinner(sq) || sq[i]) {
       return;
     }
     sq[i] = xIsNext ? "X" : "O";
-    setHistory(history.concat([{ squares: sq }]));
+    setHistory(historyAtStep.concat([{ squares: sq }]));
     setXIsNext(!xIsNext);
-    setStepNumber(history.length);
+    setStepNumber(historyAtStep.length);
   };
 
-  const jumpTo = function (step) {
+  const jumpTo = (step) => {
     setXIsNext(step % 2 === 0);
     setStepNumber(step);
   };
-
-  // const history = this.state.history;
-  const current = history[stepNumber];
-  const winner = calculateWinner(current.squares);
 
   const moves = history.map((step, move) => {
     const desc = move ? `Go back to move #${move}` : `Go to game start`;
@@ -161,7 +89,6 @@ function Game() {
     </div>
   );
 }
-// ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
 
